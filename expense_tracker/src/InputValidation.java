70b0de548dce7e9cc8d446badb86602aa17bca82
Pratyush Dubey <pratyushdubey29@github.com>
2023-09-25 19:31:42 -0400

    Added javadocs to InputValidation class.

--- expense_tracker/src/InputValidation.java
+++ expense_tracker/src/InputValidation.java
@@ -1,8 +1,21 @@
 import java.util.Arrays;
 
+/**
+ * This class is the validation class for the inputs in the expense tracker application
+ */
 public class InputValidation {
+    /**
+     * This is the minimum valid amount
+     */
     final int MIN_AMT = 0;
+    /**
+     * This is the maximum valid amount
+     */
     final int MAX_AMT = 1000;
+
+    /**
+     * This enum contains the valid categories
+     */
     enum ValidCategory {
         FOOD,
         TRAVEL,
@@ -10,10 +23,22 @@ public class InputValidation {
         ENTERTAINMENT,
         OTHER
     }
+
+    /**
+     * This method checks of the amount is in the valid range
+     * @param amt amount that is to be checked
+     * @return boolean stating if the amount is valid
+     */
     boolean isAmountValueValid(double amt) {
-        return amt >= MIN_AMT && amt <= MAX_AMT;
+        return amt > MIN_AMT && amt < MAX_AMT;
+        // NOTE: The requirements were "The amount should be greater than 0 and less than 1000"
     }
 
+    /**
+     * This method checks if the category entered is valid
+     * @param category category to be checked
+     * @return boolean stating if the category is valid
+     */
     boolean isCategoryValid(String category) {
         return Arrays.stream(ValidCategory.values()).anyMatch((t) -> t.name().equals(category.toUpperCase()));
     }
